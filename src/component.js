39b9bcfbfa6a1964ac97b4f59fe5794427c3ce32
Jody Septiawan <jody@192.168.1.11>
2021-11-16 10:38:42 +0700

    Update

--- src/component.js
+++ src/component.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React from 'react';
+
+import Header from './components/header';
 
 function Component() {
   return (
-    //   Code Here
-    <div></div>
+    <>
+      <Header />
+      <Content />
+      <Header />
+      <Header />
+      <Header />
+      <Content />
+      <Content />
+      <Content />
+    </>
   );
 }
 
-// Create a new component here
+function Content() {
+  return (
+    <div>
+      <h1>This Is A Content</h1>
+      <button>Click Me</button>
+    </div>
+  );
+}
 
 export default Component;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Component from "./component";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+import Component from './component';
 
 ReactDOM.render(
   <React.StrictMode>
     <Component />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
