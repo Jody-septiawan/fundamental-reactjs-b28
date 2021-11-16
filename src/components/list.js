6ee4fa1e2648ddd49de92173f64a27ff6045e84e
Jody Septiawan <jody@192.168.1.11>
2021-11-16 11:16:09 +0700

    Update

--- src/components/list.js
+++ src/components/list.js
@@ -1,1 +1,13 @@
-// code Here
+function List(props) {
+  const { listData, color, qty } = props;
+
+  return (
+    <>
+      <h1>
+        {listData} - Color: {color} - QTY: {qty}
+      </h1>
+    </>
+  );
+}
+
+export default List;
--- src/props.js
+++ src/props.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React from 'react';
 
 //Import Components
+import List from './components/list';
 
 function Props() {
+  const firstCarMerk = 'MBW';
+  const firstCarColor = 'pink';
+  const firstCarQty = 274;
+
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>On the image element using the default props, namely src</p>
+      <img
+        src="https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        alt="img"
+      />
+
+      <List listData={firstCarMerk} color={firstCarColor} qty={firstCarQty} />
+      <List listData="Mercedes-Benz" />
+      <List listData="Lamborghini" />
+    </div>
   );
 }
 
